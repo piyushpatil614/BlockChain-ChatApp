1e82535f09baa8b0e11be7d4200050c9c2bbb5b6
piyushpatil614 <57655955+piyushpatil614@users.noreply.github.com>
2022-07-31 11:16:22 +0530

    Update ChatAppContract.sol

--- ChatAppContract.sol
+++ ChatAppContract.sol
@@ -3,9 +3,10 @@
 pragma solidity ^0.8.0;
 
 contract Payment{
-event message(address indexed from ,string message ,address to);
-function sendMessage(address _to,string memory _message) public {
-    emit message(msg.sender,_message,_to);
+event message(address indexed from ,string message ,address to,string timestamp);
+function sendMessage(address _to,string memory _message,string memory time) public {
+
+    emit message(msg.sender,_message,_to,time);
 }
     
 }
